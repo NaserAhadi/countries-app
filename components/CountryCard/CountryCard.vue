7cfc9175fada015ef556a18b9504d0810bc25438
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 16:24:10 +0330

    FEAT: Create card component for individual country data

---
+++ components/CountryCard/CountryCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-img
+      :src="countrySpecification.flags.svg"
+      :aspect-ratio="4/3"
+    />
+    <v-card-text>
+      {{ countrySpecification.name }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CountryCard',
+  props: {
+    countrySpecification: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
