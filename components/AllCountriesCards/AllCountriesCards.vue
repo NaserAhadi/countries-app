19946837d94eee4d0d8ae21619cc63c2ad68068b
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 16:23:14 +0330

    FEAT: Create component for all countries cards

---
+++ components/AllCountriesCards/AllCountriesCards.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid class="all-countries-cards">
+    <v-row>
+      <v-col
+
+        v-for="(item, index) in countriesData"
+        :key="index"
+        cols="12"
+        md="3"
+      >
+        <CountryCard :country-specification="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CountryCard from '../CountryCard/CountryCard'
+export default {
+  name: 'AllCountriesCards',
+  components: {
+    CountryCard
+  },
+  props: {
+    countriesData: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// .all-countries-cards{
+//     width: 100%;
+// }
+</style>
