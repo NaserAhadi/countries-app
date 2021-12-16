b55dc493faf3659939d0ca71ee327db0dd61e04b
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 23:30:20 +0330

    FEAT: Create component for searching country by name

---
+++ components/SearchCountry/SearchCountry.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <BaseSearch
+      v-model="searchedValue"
+      label="search for a country"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import BaseSearch from '~/components/DesignSystem/BaseSearch/BaseSearch'
+export default {
+  name: 'SearchCountry',
+  components: {
+    BaseSearch
+  },
+  data () {
+    return {
+      searchedValue: ''
+    }
+  },
+  watch: {
+    searchedValue (value) {
+      if (value) {
+        this.getSearchedCountryInformation(value)
+      } else {
+        this.getAllCountriesInformation()
+      }
+    }
+  },
+  methods: {
+    ...mapActions('home', ['getSearchedCountryInformation', 'getAllCountriesInformation'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
