8132c30dbb6d94b30b769c56118eb56e097e9ef9
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 23:24:30 +0330

    FEAT: Create BaseCard component

---
+++ components/DesignSystem/BaseCard/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card v-bind="$attrs">
+    <slot />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
