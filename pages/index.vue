06a42b9e82eda6fa6a2033d2020bb49da3fd5a01
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 23:31:41 +0330

    FEAT: Add Searching country component and Regional filteration component to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,38 @@
 <template>
-  <v-row class="home-page">
-    <v-col cols="12" class="my-col">
-      <AllCountriesCards
-        :countries-data="allCountriesInformation"
-      />
-    </v-col>
-  </v-row>
+  <v-container fluid class="home-page">
+    <v-row class="d-flex justify-space-between px-3">
+      <v-col cols="4">
+        <SearchCountry />
+      </v-col>
+      <v-col cols="3">
+        <RegionFilter />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="my-col">
+        <AllCountriesCards
+          :countries-data="countriesInformation"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
 import AllCountriesCards from '~/components/AllCountriesCards/AllCountriesCards'
+import RegionFilter from '~/components/RegionFilter/RegionFilter'
+import SearchCountry from '~/components/SearchCountry/SearchCountry'
+
 export default {
   name: 'IndexPage',
   components: {
-    AllCountriesCards
+    AllCountriesCards,
+    RegionFilter,
+    SearchCountry
   },
   computed: {
-    ...mapState('home', ['allCountriesInformation'])
+    ...mapState('home', ['countriesInformation'])
   },
   mounted () {
     this.getAllCountriesInformation()
