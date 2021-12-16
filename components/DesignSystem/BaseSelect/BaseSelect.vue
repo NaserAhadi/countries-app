7db0394322507174e30b895e68fac23714635676
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-16 23:25:23 +0330

    FEAT: Create BaseSelect component

---
+++ components/DesignSystem/BaseSelect/BaseSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="BaseSelect">
+    <v-select
+      outlined
+      v-bind="$attrs"
+      :items="inputItems"
+      @change="sendOutSelectedValue($event)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSelect',
+  props: {
+    inputItems: {
+      type: Array,
+      default: () => {}
+    }
+  },
+  methods: {
+    sendOutSelectedValue (value) {
+      this.$emit('selected-value', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./BaseSelect.scss";
+</style>
