ae3821ac98a201b5ec7eafa9189513109cda15a8
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-17 16:28:39 +0330

    FEAT: Add arrow icon

--- assets/svg/svgRegisteration.js
+++ assets/svg/svgRegisteration.js
@@ -1,6 +1,11 @@
 import SearchLight from '@/assets/svg/icon/search/SearchLight.svg?inline'
+import ArrowLeftLight from '@/assets/svg/icon/arrow/ArrowLeftLight.svg?inline'
 
 export const searchIcons = {
   light: SearchLight,
   dark: null
+}
+
+export const arrowIcon = {
+  light: ArrowLeftLight
 }
