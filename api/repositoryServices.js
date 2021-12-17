1073ce5c0348269256900301d568b379bc493dc5
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-12-17 16:27:41 +0330

    FEAT: Add detail page api to repository services

--- api/repositoryServices.js
+++ api/repositoryServices.js
@@ -1,7 +1,7 @@
 import homePageApi from './homePageApi'
-// import detailPageApi from './detailPageApi'
+import detailPageApi from './detailPageApi'
 
 export default $axios => ({
-  homePageService: homePageApi($axios)
-  // detailPageService: detailPageApi($axios)
+  homePageService: homePageApi($axios),
+  detailPageService: detailPageApi($axios)
 })
